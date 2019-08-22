e6d649756b5af49d36711f7d7600764fdf78df1a
AJ-Hagemann <54418965+AJ-Hagemann@users.noreply.github.com>
2019-08-22 16:24:41 -0500

    Milestone 2

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,5 +1,16 @@
+/**
+ * Class: 44-242 Data Structures
+ * Author: AJ Hagemann
+ * Description: Testing your tools
+ * Due: Aug-22-19
+ * I pledge that I have completed the programming assignment independently.
+   I have not copied the code from a student or any source.
+   I have not given my code to any other student.
+   I have not given my code to any other student and will not share this code
+   with anyone under any circumstances.
+*/
 public class Foo {
     public static int bar() {
-        return -1;
+        return 0;
     }
 }
